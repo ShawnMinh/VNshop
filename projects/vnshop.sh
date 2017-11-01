1f7ecc19a4ba6048008e26fd768f68be67604179
ShawnMinh <thisisming@qq.com>
2017-11-01 19:32:41 +0800

    new file

--- projects/vnshop.sh
+++ projects/vnshop.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
  
 WEB_PATH='/home/wwwroot/vnshop/'
-WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
+WEB_PATH_CLIENT='/home/wwwroot/omgwhatsthat.top/shop/client'
 WEB_USER='www'
 WEB_USERGROUP='www'
- 
-echo "Start deployment shudong.wang"
+# we can do 
+echo "Start deployment vx.itnote.cn"
 cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/release
 # git clean -f
+# 把项目拉取到最新
 git pull
 git checkout master
 echo "changing permissions..."
+# 切换到client里面
 cd $WEB_PATH_CLIENT
+# 把vue项目编译
 npm run build
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH_CLIENT
 echo "Finished."
